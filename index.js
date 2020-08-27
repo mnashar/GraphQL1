245ddfd358f30fbeb261f253d640d5b25041d0ac
your name here <msn@aucegypt.edu>
2020-08-27 10:00:43 -0400

    GraphQL

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require ("express");
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/",(req,res)=>res.send("Hello World!"));
-
+app.get("/", (req, res) => res.send("HEY World!"));
+app.use(bodyParser.json());
 app.listen(5000, () => console.log("Server is running on port 5000"));
